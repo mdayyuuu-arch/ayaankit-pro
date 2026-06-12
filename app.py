ca353221e05fafe4c3ca523ad99732951fb5d334
Mohammed Ayaan <your@email.com>
2026-06-13 03:08:40 +0530

    Fix host for deployment

--- app.py
+++ app.py
@@ -194,6 +194,6 @@ def history():
     return base(content)
 
 if __name__ == "__main__":
-    print("[*] AyaanKit SaaS: http://127.0.0.1:5000")
+    print("[*] AyaanKit SaaS: http://0.0.0.0:5000")
     print("[*] Login: ayaan / admin123")
-    app.run(host="127.0.0.1", port=5000, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
